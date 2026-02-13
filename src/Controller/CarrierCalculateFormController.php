e985bf007480efca958c283e2b5308402207ea80
Romchik38 <pomahehko.c@gmail.com>
2026-02-13 17:16:45 +0200

    controller uses a service

--- src/Controller/CarrierCalculateFormController.php
+++ src/Controller/CarrierCalculateFormController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Application\CarrierService\CarrierService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CarrierCalculateFormController extends AbstractController
 {
+    public function __construct(
+        private readonly CarrierService $carrierService
+    ) {
+    }
+
     #[Route('/carrier/calculate/form', name: 'app_carrier_calculate_form')]
     public function index(): Response
     {
