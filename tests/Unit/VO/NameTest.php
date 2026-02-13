98fd10b7d1e88908df73ecd093c6d09e7c1ebd39
Romchik38 <pomahehko.c@gmail.com>
2026-02-13 13:25:22 +0200

    tests: carrier

---
+++ tests/Unit/VO/NameTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\VO;
+
+use App\Domain\Carrier\VO\Name;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class NameTest extends TestCase
+{
+    public function testEmptyString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Name('');
+    }
+}
---
+++ tests/Unit/VO/SlugTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\VO;
+
+use App\Domain\Carrier\VO\Slug;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class SlugTest extends TestCase
+{
+    public function testEmptyString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Slug('');
+    }
+}
