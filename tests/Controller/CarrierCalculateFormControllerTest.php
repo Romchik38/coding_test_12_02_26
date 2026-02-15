4ee8cd95c3ed08ca28cc3ac9f934a525eeae8235
Romchik38 <pomahehko.c@gmail.com>
2026-02-15 16:44:29 +0200

    app tests are ready

--- tests/Controller/CarrierCalculateFormControllerTest.php
+++ tests/Controller/CarrierCalculateFormControllerTest.php
@@ -36,4 +36,48 @@ class CarrierCalculateFormControllerTest extends WebTestCase
         $this->assertSame('EUR', $result[CalculateView::CURRENCY_FIELD]);
         $this->assertSame($weight, $result[CalculateView::PRICE_FIELD]);
     }
+
+    public function testCalculateShippingCostsErrorSlug(): void
+    {
+        $slugField = CalculateCommand::slugField;
+        $weightField  = CalculateCommand::weightField;
+        $carrierSlug = 'testcompany10';  // wrong
+        $weight = 10;
+        $client = static::createClient();
+        $client->request('POST', '/api/shipping/calculate', [
+            $weightField => $weight,
+            $slugField => $carrierSlug,
+        ]);
+
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+
+        $content = $client->getResponse()->getContent();
+        $responseData = json_decode($content, true);
+        $status = $responseData[Dto::STATUS_FIELD];
+
+        $this->assertSame(Dto::ERROR_FIELD, $status);
+    }
+
+    public function testCalculateShippingCostsErrorWeight(): void
+    {
+        $slugField = CalculateCommand::slugField;
+        $weightField  = CalculateCommand::weightField;
+        $carrierSlug = 'testcompany1';
+        $weight = 0; // wrong
+        $client = static::createClient();
+        $client->request('POST', '/api/shipping/calculate', [
+            $weightField => $weight,
+            $slugField => $carrierSlug,
+        ]);
+
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+
+        $content = $client->getResponse()->getContent();
+        $responseData = json_decode($content, true);
+        $status = $responseData[Dto::STATUS_FIELD];
+
+        $this->assertSame(Dto::ERROR_FIELD, $status);
+    }
 }
